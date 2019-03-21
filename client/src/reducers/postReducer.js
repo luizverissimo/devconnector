a2d34231b2725cadadb7cebb1043b6d3e99f7590
Luiz Verissimo <luizverissimosouza@gmail.com>
2019-03-21 08:40:37 -0300

    add(Posts)

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,41 @@
+import {
+  ADD_POST,
+  GET_ERRORS,
+  POST_LOADING,
+  GET_POSTS,
+  DELETE_POST
+} from "../actions/types";
+
+const intialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = intialState, action) {
+  switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
+    default:
+      return state;
+  }
+}
